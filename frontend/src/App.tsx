ddee6c5e0d0e5cfd993fe3915e22d39a302d8cac
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-04-16 11:26:22 +0200

    ciFix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginRegisterForm from "./components/LoginRegisterForm";
-import TestPage from "./components/TestPage";
 
 export default function App() {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path={'/'} element={<LoginRegisterForm/>}/>
-                <Route path={'/test'} element={<TestPage/>}/>
             </Routes>
         </BrowserRouter>
     )
