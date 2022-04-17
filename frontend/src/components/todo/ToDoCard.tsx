e8ca41a56b1b55abb410ed7be6ebdb05e1e11722
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-04-17 12:43:28 +0200

    actionarea for cards

--- frontend/src/components/todo/ToDoCard.tsx
+++ frontend/src/components/todo/ToDoCard.tsx
@@ -1,20 +1,73 @@
-import {Card, CardContent, Typography} from "@mui/material";
+import {Card, CardActions, CardContent, Collapse, IconButton, IconButtonProps, styled, Typography} from "@mui/material";
 import {TodoItem} from "../../interfaces/todo";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import EditIcon from '@mui/icons-material/Edit';
+import {useState} from "react";
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
 
 export default function ToDoCard(props:{infos:TodoItem}){
+
+    const [expanded, setExpanded] = useState(false)
+
+    const ExpandMore = styled((props: ExpandMoreProps) => {
+        const { expand, ...other } = props;
+        return <IconButton {...other} />;
+    })(({ theme, expand }) => ({
+        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    }));
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return(
-        <Card sx={{margin:'0.1rem', width:'90%',marginLeft:'5%'}}>
+        <Card sx={{margin:'0.1rem', width:'90%',marginLeft:'5%'}} variant={'outlined'}>
             <CardContent>
                 <Typography variant="h5" component="div">
                     {props.infos.task}
                 </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    {props.infos.assigned}
-                </Typography>
-                <Typography variant="body2">
-                    {props.infos.description}
-                </Typography>
             </CardContent>
+            <CardActions disableSpacing>
+                <IconButton>
+                    <ArrowBackIosIcon sx={{margin:'0 2rem 0 0'}}/>
+                </IconButton>
+                <IconButton>
+                    <EditIcon/>
+                </IconButton>
+                <IconButton>
+                    <ArrowForwardIosIcon sx={{margin:'0  0 0 2rem'}}/>
+                </IconButton>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Beschreibung:</Typography>
+                    <Typography paragraph>
+                        {props.infos.description}
+                    </Typography>
+                    <Typography paragraph>Wer erledigt das:</Typography>
+                    <Typography paragraph>
+                        {props.infos.assigned}
+                    </Typography>
+                </CardContent>
+            </Collapse>
         </Card>
     )
 }
